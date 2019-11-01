9e6744466ea1d25b0037e5235673f1a8a845ba15
CarnunMP <carnun@hotmail.co.uk>
2019-11-01 20:55:46 +0000

    client built out and connected (?)

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [dummyData, setDummyData] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${(process.env.REACT_APP_API || '')}/api/dummy`)
+      .then((res) => {
+        setDummyData(res.dummyData);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {dummyData.map((item) => (
+            item.text
+          ))}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
