579d94f7d970b6fa0454d298fca5b7ab84e3ac85
CarnunMP <carnun@hotmail.co.uk>
2019-11-03 17:13:25 +0000

    cra app trimmed down some; reset added to index.css; less up and running

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -18,24 +17,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {dummyData && dummyData.map((item) => (
-            item.text
-          ))}
-        </a>
-      </header>
+      {dummyData && dummyData.map((item) => (
+        item.text
+      ))}
     </div>
   );
 }
