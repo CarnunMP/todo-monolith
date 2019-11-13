343163bb1a5a3f8906a9de7e05eebfd4a66e920f
CarnunMP <carnun@hotmail.co.uk>
2019-11-13 20:21:33 +0000

    fleshed-out seed files

--- data/seeds/02-todos.js
+++ data/seeds/02-todos.js
@@ -1,13 +1,22 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('todos').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('todos').insert([
+        {id: 1, text: 'finish report'},
+        {id: 2, text: 'send email to boss'},
+        {id: 3, text: 'write up draft of article'},
+        {id: 4, text: 'fix bug'},
+        {id: 5, text: 'research bike frames'},
+        {id: 6, text: 'read up more on Zettelkasten'},
+        {id: 7, text: 'organise room'},
+        {id: 8, text: 'take do out for a poo'},
+        {id: 9, text: 'do pushups'},
+        {id: 10, text: 'cook dinner'},
+        {id: 11, text: 'call friend'},
+        {id: 12, text: 'think about xmas prezzies for fam'}
       ]);
     });
 };
